0c2471b6218ec56507529790ad13ed1cbe492350
Roma Volkov <Drakyla60@gmail.com>
2021-09-28 22:33:44 +0300

    Виніс лоігку в функцію.

--- 01.php
+++ 01.php
@@ -6,28 +6,33 @@ $items = [
     ['count' => 1, 'price' => 122],
 ];
 
-// Вартість
-$cost = 0;
+function calculateCost($items)
+{
+    // Вартість
+    $cost = 0;
 
-// Сумуємо всі товари
-foreach ($items as $item) {
-    $cost += $item['price'] * $item['count'];
-}
-//Якщо є товар від 100 до 150 грн то робимо знижку 9%
-foreach ($items as $item) {
-    if (100 <= $item['price'] && $item['price'] <= 150) {
-        $cost -= (9 / 100) * $item['price'] * $item['count'];
+    // Сумуємо всі товари
+    foreach ($items as $item) {
+        $cost += $item['price'] * $item['count'];
     }
-}
-//Якщо сьогодні 15 число то робимо знижку 5%
-if (date('d') == 28) {
-    $cost = 0.95 * $cost;
+    //Якщо є товар від 100 до 150 грн то робимо знижку 9%
+    foreach ($items as $item) {
+        if (100 <= $item['price'] && $item['price'] <= 150) {
+            $cost -= (9 / 100) * $item['price'] * $item['count'];
+        }
+    }
+    //Якщо сьогодні 15 число то робимо знижку 5%
+    if (date('d') == 28) {
+        $cost = 0.95 * $cost;
+    }
+
+    //Якщо сума товарів більше ніж 1000 грн то робимо знижку 7%
+    if ($cost >= 1000) {
+        $cost = 0.93 * $cost;
+    }
+    return $cost;
 }
 
-//Якщо сума товарів більше ніж 1000 грн то робимо знижку 7%
-if ($cost >= 1000) {
-    $cost = 0.93 * $cost;
-}
 //Результат
-echo $cost . PHP_EOL;
+echo calculateCost($items) . PHP_EOL;
 
