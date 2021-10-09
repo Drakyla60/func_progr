123e385a4a4a67c17f74be698bbe9af00a421c1a
Roma Volkov <Drakyla60@gmail.com>
2021-10-09 19:21:52 +0300

    Формуємо список сторінок

--- parser/01.php
+++ parser/01.php
@@ -3,6 +3,15 @@
 use Symfony\Component\DomCrawler\Crawler;
 
 require __DIR__ . '/vendor/autoload.php';
+#######Config
+
+$forumUrl = './viewforum.php?f=28';
+
+#######
+
+function first($array) {
+    return reset($array);
+}
 
 function normalizeUrl($url): string
 {
@@ -14,17 +23,38 @@ function getHtml($url)
     return file_get_contents(normalizeUrl($url));
 }
 
-$forumUrl = './viewforum.php?f=28';
+function clearUrl($url) {
+    return preg_replace('#\&sid=.{32}#s', '', $url);
+}
 
-$html = getHtml($forumUrl);
+function getForumMaxPageNumber($forumUrl) {
 
-$crawler = new Crawler($html);
+    $html = getHtml($forumUrl);
+    $crawler = new Crawler($html);
 
-$page = $crawler
-    ->filter('div.action-bar.bar-top .pagination li:nth-last-of-type(2)')
-    ->each(function (Crawler $link) {
-        return intval($link->text());
-    });
+    $page = $crawler
+        ->filter('div.action-bar.bar-top .pagination li:nth-last-of-type(2)')
+        ->each(function (Crawler $link) {
+            return intval($link->text());
+        });
 
-echo max(reset($page), 1);
+    return max(reset($page), 1);
+}
+
+function getForumPages($forumUrl): array
+{
+    echo 'Forum pages for' . clearUrl($forumUrl). PHP_EOL;
+    $getMaxPageNumber = getForumMaxPageNumber($forumUrl);
+    $result = [];
+    foreach (range(1, $getMaxPageNumber) as $number) {
+        $result[] = $forumUrl . ($number > 1 ? '&start=' . (25 * ($number - 1)) : '');
+    }
+    return $result;
+}
+
+echo clearUrl(print_r(getForumPages($forumUrl)));
+
+
+//echo getForumMaxPageNumber($forumUrl);
+
 echo PHP_EOL;
